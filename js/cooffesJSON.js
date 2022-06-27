5757912f5812e2d8b1635bf197fcaa9adb1de3ae
Matheus Guedes <matheusguedessz@gmail.com>
2022-06-27 08:29:27 +0100

    commit-updateModal

---
+++ js/cooffesJSON.js
@@ -0,0 +1,7 @@
+let produtos = [
+    {id: 1, nome: 'Copo com Prensa Francesa' , url:'./assets/coador.svg'},
+    {id: 2, nome: 'Coador de café (3 xícaras)' , url:'./assets/coador.svg'},
+    {id: 3, nome: 'Coador de café (3 xícaras)' , url:'./assets/coador.svg'},
+    {id: 4, nome: 'Coador de café (3 xícaras)' , url:'./assets/coador.svg'},
+    {id: 5, nome: 'Coador de café (3 xícaras)' , url:'./assets/coador.svg'}
+]
--- js/modules/kits.js
+++ js/modules/kits.js
@@ -7,11 +7,45 @@ let bancoDeProdutos = [
 ]   
 
 export default function initKits(){
+    let modalQt = 1;
+    function loadProducts(){
     bancoDeProdutos.map((produto, index)=>{    
         let produtoClone = document.querySelector('.models .produto').cloneNode(true)
         document.querySelector('.produtos__container').append(produtoClone)
         produtoClone.querySelector('.produto__img').src = produto.url;
         produtoClone.querySelector('.produto__nome').innerText = produto.nome;
         produtoClone.querySelector('.produto__preco').innerText = `R$ ${produto.preco.toFixed(2)}`;
+      })
+    }
+    loadProducts();
+    function openModalProduct(){
+    let btnIncremento = document.querySelector('[data-modal="incremento"]')
+    let arrayProducts = Array.from(document.querySelectorAll('.produtos__container .produto'))
+    arrayProducts.map((item, index)=>{
+        item.addEventListener('click', openModal)
+        let indexProduct = index;
+        let modalProduct = document.querySelector('.produto-modal')
+        function openModal(){
+            modalProduct.classList.add("active")
+            updateModal();
+        }
+        document.querySelector('.btnclose__produto-modal').addEventListener('click', ()=>{
+            modalProduct.classList.remove("active")
+        })
+        function updateModal(){
+            document.querySelector('.produto-modal__nome').innerText = bancoDeProdutos[indexProduct].nome;
+            document.querySelector('.produto-modal__preco').innerText = `R$ ${bancoDeProdutos[indexProduct].preco.toFixed(2)}`
+            document.querySelector('.produto-modal-img__img').src = bancoDeProdutos[indexProduct].url
+            document.querySelector('.produto-modal__qt-numero').innerText = modalQt;
+        }
+    }) 
+    btnIncremento.addEventListener('click', (e)=>{
+        e.preventDefault();
+        modalQt++;
+        document.querySelector('.produto-modal__qt-numero').innerText = modalQt;
     })
+    }   
+   openModalProduct();
 } 
+
+
